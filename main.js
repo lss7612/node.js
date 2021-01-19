dfab146ccb58aec267f691a031afaa50b03e44d9
lss7612 <lss76121@naver.com>
2021-01-19 21:58:24 +0900

    0119

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var url = require('url');
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  console.log(queryData.id);
+  var title = queryData.id;
   if (_url == '/') {
-    url = '/index.html';
+    title = 'Welcome';
   }
   if (_url == '/favicon.ico') {
     response.writeHead(404);
@@ -14,7 +14,28 @@ var app = http.createServer(function (request, response) {
     return;
   }
   response.writeHead(200);
-  console.log(__dirname + _url);
-  response.end(queryData.id);
+  fs.readFile(`data/${queryData.id}`, 'UTF8', function (err, description) {
+    var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}
+        </p>
+      </body>
+      </html>
+    `;
+    response.end(template);
+  });
 });
 app.listen(3000);
